cbde03632205d4bb66e50b8513f279c344cbe43b
hiagor <hiagob.reis@hotmail.com>
2019-09-23 20:30:04 -0300

    Add project files.

---
+++ Fazenda Serrana/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fazenda_Serrana.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
