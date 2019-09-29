695a27a84914c8b6941d6abac1cf1ff33ce6f34d
hiagor <hiagob.reis@hotmail.com>
2019-09-28 22:07:08 -0300

    Começo do mapeamento

---
+++ Fazenda Serrana/Models/FluentMap/AreaMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fazenda_Serrana.Models.FluentMap
+{
+    public class AreaMap : IEntityTypeConfiguration<Area>
+    {
+        public void Configure(EntityTypeBuilder<Area> builder)
+        {
+            builder.ToTable("TB_AREA");
+
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            builder.Property(a => a.Tamanho)
+                .IsRequired();
+
+            builder.HasOne(a => a.Funcionario)
+                 .WithMany(f => f.)
+                 .HasForeignKey(k => k.IdFuncionario);
+        }
+    }
+}
---
+++ Fazenda Serrana/Models/FluentMap/FuncionarioMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fazenda_Serrana.Models.FluentMap
+{
+    public class FuncionarioMap : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            builder.ToTable("TB_FUNCIONARIO");
+
+            builder.HasKey(f => f.Matricula);
+            builder.Property(f => f.Matricula)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            builder.Property(f => f.Nome)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(f => f.DataAdmissao)
+                .IsRequired();
+
+            builder.Property(f => f.Cargo)
+                .IsRequired();
+
+            
+        }
+
+    }
+}
